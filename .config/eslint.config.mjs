72feeed09295effe85667cf431be1e376c52c926
amoralesc <alejandro.morales@meliusid.com>
2025-02-11 23:23:26 +0000

    add tooling

---
+++ .config/eslint.config.mjs
@@ -0,0 +1,12 @@
+// ESLint configuration file.
+// https://eslint.org/docs/latest/use/configure/configuration-files
+
+import json from "eslint-plugin-json";
+
+export default [
+  // JSON files rules.
+  {
+    files: ["**/*.json"],
+    ...json.configs["recommended-with-comments"],
+  },
+];
