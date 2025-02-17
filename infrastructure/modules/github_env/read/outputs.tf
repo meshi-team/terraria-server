8a8148ea894b211eac84a5c782948f4ba2506411
amoralesc <alejandro.morales@meliusid.com>
2025-02-17 01:32:51 +0000

    use tomap function

--- infrastructure/modules/github_env/read/outputs.tf
+++ infrastructure/modules/github_env/read/outputs.tf
@@ -4,11 +4,11 @@ locals {
 
 output "variables" {
   description = "Variables from the GitHub Actions environment"
-  value = map(
-    "SERVER_MEMORY_IN_GBS", local.github_env_vars["SERVER_MEMORY_IN_GBS"],
-    "SERVER_OCPUS", local.github_env_vars["SERVER_OCPUS"],
-    "SERVER_PORT", local.github_env_vars["SERVER_PORT"],
-    "SERVER_SSH_PUBLIC_KEY", local.github_env_vars["SERVER_SSH_PUBLIC_KEY"],
-    "SERVER_SUBDOMAIN", local.github_env_vars["SERVER_SUBDOMAIN"],
-  )
+  value = tomap({
+    "SERVER_MEMORY_IN_GBS"  = local.github_env_vars["SERVER_MEMORY_IN_GBS"],
+    "SERVER_OCPUS"          = local.github_env_vars["SERVER_OCPUS"],
+    "SERVER_PORT"           = local.github_env_vars["SERVER_PORT"],
+    "SERVER_SSH_PUBLIC_KEY" = local.github_env_vars["SERVER_SSH_PUBLIC_KEY"],
+    "SERVER_SUBDOMAIN"      = local.github_env_vars["SERVER_SUBDOMAIN"],
+  })
 }
