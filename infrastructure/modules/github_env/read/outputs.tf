09dc351bacba475c149e328668ea32d03bdd2997
amoralesc <alejandro.morales@meliusid.com>
2025-02-17 01:26:31 +0000

    refactor github_env.read module to output variables as map

--- infrastructure/modules/github_env/read/outputs.tf
+++ infrastructure/modules/github_env/read/outputs.tf
@@ -2,27 +2,13 @@ locals {
   github_env_vars = { for v in data.github_actions_variables.variables.variables : v.name => v.value }
 }
 
-output "server_memory_in_gbs" {
-  description = "Amount of memory in GBs for the server instance"
-  value       = try(local.github_env_vars["SERVER_MEMORY_IN_GBS"], null)
-}
-
-output "server_ocpus" {
-  description = "Number of OCPUs for the server instance"
-  value       = try(local.github_env_vars["SERVER_OCPUS"], null)
-}
-
-output "server_port" {
-  description = "Port for the server"
-  value       = try(local.github_env_vars["SERVER_PORT"], null)
-}
-
-output "server_ssh_public_key" {
-  description = "SSH public key for the server instance"
-  value       = try(local.github_env_vars["SERVER_SSH_PUBLIC_KEY"], null)
-}
-
-output "server_subdomain" {
-  description = "Subdomain for the server"
-  value       = try(local.github_env_vars["SERVER_SUBDOMAIN"], null)
+output "variables" {
+  description = "Variables from the GitHub Actions environment"
+  value = map(
+    "SERVER_MEMORY_IN_GBS", local.github_env_vars["SERVER_MEMORY_IN_GBS"],
+    "SERVER_OCPUS", local.github_env_vars["SERVER_OCPUS"],
+    "SERVER_PORT", local.github_env_vars["SERVER_PORT"],
+    "SERVER_SSH_PUBLIC_KEY", local.github_env_vars["SERVER_SSH_PUBLIC_KEY"],
+    "SERVER_SUBDOMAIN", local.github_env_vars["SERVER_SUBDOMAIN"],
+  )
 }
