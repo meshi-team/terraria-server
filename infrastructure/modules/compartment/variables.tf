88252a98f7826b90cda6f8af39023174c7101610
amoralesc <alejandro.morales@meliusid.com>
2025-02-17 01:51:00 +0000

    create compartment tf module

---
+++ infrastructure/modules/compartment/variables.tf
@@ -0,0 +1,14 @@
+variable "parent_compartment_id" {
+  description = "The OCID of the parent compartment"
+  type        = string
+}
+
+variable "name" {
+  description = "The name of the compartment"
+  type        = string
+}
+
+variable "description" {
+  description = "The description of the compartment"
+  type        = string
+}
